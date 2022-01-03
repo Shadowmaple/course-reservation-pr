0f816098ce08020c94157a48bd1936f4932d976d
Shadowmaple <1142319190@qq.com>
2022-01-03 16:49:44 +0800

    :star: update card-pages

--- miniprogram/pages/card/card.js
+++ miniprogram/pages/card/card.js
@@ -1,11 +1,23 @@
 // pages/card/card.js
+
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    hasData: true,
+    balance: 0,
+    records: [
+      {
+        type: 1, // 0->新增积分，1->减少积分
+        value: 20, // 增减积分数
+        event: "预约课程 计算机网络 成功" // 发生事件
+      },
+      {
+        type: 0, // 0->新增积分，1->减少积分
+        value: 30, // 增减积分数
+        event: "取消预约课程 操作系统 成功" // 发生事件
+      },
+    ]
   },
 
   /**
@@ -37,13 +49,6 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
@@ -56,11 +61,4 @@ Page({
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
