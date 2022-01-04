d60408aa75f4ccf432937b72999b96fa893f5cca
sparrowxw <1421937204@qq.com>
2022-01-04 14:22:12 +0800

    index jump to course

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,7 +7,9 @@ Page({
   data: {
     course: "高等数学",
     teacher: "李永乐",
-    date: "2022-1-1 ~ 2022-2-2"
+    date: "2022-1-1 ~ 2022-2-2",
+    course_id:"233",
+    url:"../course/info"
   },
 
   click_course: function () {
@@ -15,11 +17,13 @@ Page({
       url: app.globalData.apiHost + app.globalData.apiPath.courseListPath,
       method: "post",
       success: res => {
-        this.data.course = "",
+        for (i = 0; i < res.data.data.toal; i++)
         this.setData({
           course:res.data.data.list.course_name,
           teacher:res.data.data.list.teacher_name,
-          date:res.data.data.list.start_time + "~" + res.data.data.list.end_time
+          date:res.data.data.list.start_time + "~" + res.data.data.list.end_time,
+          course_id:res.data.data.list[i].course,
+          url:"../course/info?course_id=" + course_id
         })
       },
       fail: res => {
