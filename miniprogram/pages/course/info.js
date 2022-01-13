5a2628d5fefe564db5b3496b8d5c8a5189f6aae8
Shadowmaple <1142319190@qq.com>
2022-01-13 10:35:36 +0800

    update page

--- miniprogram/pages/course/info.js
+++ miniprogram/pages/course/info.js
@@ -129,7 +129,7 @@ Page({
           return
         }
         var list = new Array
-        if (this.data.page > 0) {
+        if (page > 0) {
           list = this.data.commentList
         }
         for (let i in resp.data.list) {
